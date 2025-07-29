0e9debc8de72cad21c5419154a110dc45c1d1b1e
Oleksandr_Rudniev_PD14 <st9354750@stud.duikt.edu.ua>
2025-07-29 17:33:13 +0300

    responding with json on /api/whoami get request

--- index.js
+++ index.js
@@ -24,6 +24,18 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function (req, res) {
+  const reqIP = req.ip;
+  const lang = req.headers['accept-language'];
+  const userAgent = req.headers['user-agent'];
+
+  res.json({
+    ipadress: reqIP,
+    language: lang,
+    software: userAgent
+  });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
